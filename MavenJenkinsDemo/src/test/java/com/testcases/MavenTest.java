352ae37e9323328f9ed7ccf04827ff17d96f07da
Manoj <manoja5006@gmail.com>
2020-05-03 10:53:51 +0530

    Maven Jenkins Test

---
+++ MavenJenkinsDemo/src/test/java/com/testcases/MavenTest.java
@@ -0,0 +1,32 @@
+package com.testcases;
+
+import org.testng.annotations.Test;
+
+public class MavenTest {
+
+	
+	@Test
+	public void testCase1() {
+		
+		System.out.println("First Test Case");
+	}
+	
+	@Test
+	public void testCase2() {
+		
+		System.out.println("First Test Case");
+	}
+	
+	@Test
+	public void testCase3() {
+		
+		System.out.println("First Test Case");
+	}
+	
+
+	@Test
+	public void testCase4() {
+		
+		System.out.println("Fourth Test Case");
+	}
+}
